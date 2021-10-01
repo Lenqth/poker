c85959aebb67cda4a87632c8d0d821501b784d1b
Lenqth <nullptr00@gmail.com>
2021-10-01 20:40:04 +0900

    mainを総当りにした

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::collections::HashMap;
 
-use poker::poker::{
-    hand::PokerHands,
-    payout::{Payout, JB96},
-    utils::random_gen,
-};
+use itertools::Itertools;
+use poker::poker::{cards::Cards, hand::PokerHands, payout::{Payout, JB96}, utils::random_gen};
+
 
 #[macro_use]
 extern crate timeit;
@@ -15,15 +13,23 @@ fn main() {
         let payout_calc = JB96 {};
         let mut total = 0.0f64;
         let mut cnt: HashMap<&str, i32> = HashMap::new();
-        for _ in 0..1000000 {
-            let cards = random_gen();
+        let mut c = 0;
+        
+        for card_ids in (0..52).combinations(5) {
+            c += 1;
+            let mut cards = [Cards::Heart(0); 5];
+
+            for (i,x) in card_ids.iter().enumerate() {
+                cards[i] = Cards::from_id(*x);
+            }
+
             let hand = PokerHands::cards_to_hand(&cards);
             let v = payout_calc.payout(&hand);
             total += v;
             let hand_name = payout_calc.name(&hand);
             *cnt.entry(&hand_name).or_insert(0) += 1;
         }
-        println!("Total : {}", total);
+        println!("Total : {} / {}", total , c);
         println!("{:#?}", cnt);
 })
 }
